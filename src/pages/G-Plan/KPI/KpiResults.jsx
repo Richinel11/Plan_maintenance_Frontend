59e72d0dca02c6e194bc3d3606f012596aa5c336
Blanche <mekemrichinel@gmail.com>
2026-06-30 12:09:42 +0100

    fix: update name kpi

--- src/pages/G-Plan/KPI/KpiResults.jsx
+++ src/pages/G-Plan/KPI/KpiResults.jsx
@@ -3,7 +3,7 @@ import { COLORS, styles } from "./components/kpi";
 import { DonutChart, MiniBar, BarChart } from "./components/charts";
 
 import Page1 from './KPI1/Kpi1';
-import Page2 from './KPI2/KPI2';
+import Page2 from './KPI2/Kpi2';
 import Page3 from './KPI3/Kpi3';
 import Page4 from './KPI4/Kpi4';
 import Page5 from './KPI5/Kpi5';
